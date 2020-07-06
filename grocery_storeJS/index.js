9f52ffece82485137ada822b5e9789693c3f06af
Samantha Allen <samantha.allen@bryanuniversity.edu>
2020-07-05 22:04:18 -0500

    testing

---
+++ grocery_storeJS/index.js
@@ -0,0 +1,18 @@
+var name = "Sam"
+var age = 21
+var isAlive = true
+var groceryCart = ["milk", "eggs", "bread", "lemonade", "orangejuice", "cheese"]
+
+var shopper = {
+    firstName: "Dude",
+    lastName: "Guy",
+    age: "31",
+    hairColor: "brown",
+    isAlive: "true",
+    mentallyStable: "false",
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+console.log("My name is" + " " + shopper.fullName())
