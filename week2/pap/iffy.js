6123a9e6112e53c343cb842362bb6f32aac8547d
Samantha Allen <samantha.allen@bryanuniversity.edu>
2020-07-12 20:37:25 -0500

    pap redo

--- week2/pap/iffy.js
+++ week2/pap/iffy.js
@@ -15,10 +15,8 @@ if(5 > 3){
     console.log("is greater than")
 }
 
-var cat = {
-    length: 3
-}
-if(cat.length = 3){
+var myWord = "cat"
+if (myWord.length == 3){
     console.log("is the length")
 }
 
@@ -39,18 +37,44 @@ if(person.name.startsWith("B")){
 
 if(person.name.startsWith("B") && person.age >= 18){
     console.log("is allowed in the movie")
-} else if(person.name.startsWith("B") && person.age < 18){
+} else(person.name.startsWith("B") && person.age < 18) 
     console.log("is not allowed in the movie")
-}
 
 if(1 === 1){
     console.log("strict")
 } else if("1" == 1){
-    console.log("strict")
+    console.log("loose")
 } else {
     ("not equal at all")
 }
 
 if(1 <= 2 && 2 === 4){
     console.log("yes")
+}
+
+console.log(typeof "dog");
+console.log(typeof true);
+if (typeof "dog" === undefined){
+    console.log("this is undefined")
+} else {
+    console.log("this is defined")
+}
+
+if ("s" > 12){
+    console.log("s is greater than 12")
+} else {
+    console.log("s is not greater than 12")
+}
+
+if ("a" < "d"){
+    console.log("a is less than d")
+} else {
+    console.log("a is not less than d")
+}
+
+value = 1
+if (value % 2 === 0){
+    console.log("is even")
+} else {
+    console.log("is odd")
 }
