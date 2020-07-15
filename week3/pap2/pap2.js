1599066a6c5770f7d095a5a8d52b9c635f9533fe
Samantha Allen <samantha.allen@bryanuniversity.edu>
2020-07-15 11:10:34 -0500

    pap2

---
+++ week3/pap2/pap2.js
@@ -0,0 +1,61 @@
+//practice
+
+//function sum(num1, num2){
+//    return num1 + num2
+//}
+
+// var result = sum(10, 5)
+// console.log(result)
+
+//function myFunc(data){
+//    console.log(data)
+//}
+
+//myFunc(1)
+//myFunc("hello")
+//myFunc(true)
+
+//function loopThroughArray(array){
+//    for(var i =0; i < array.length; i++){
+//        console.log(array[i])
+//    }
+//}
+
+//loopThroughArray([1, 2, 3, 4])
+
+//actual assignment
+function plus(num1, num2){
+    return num1 + num2
+}
+
+var result = plus(420, 69)
+console.log(result)
+
+function findMax(num1, num2, num3){
+    return Math.max(num1, num2, num3)
+}
+
+var a = findMax(3, 4, 100)
+console.log(a)
+
+function oddOrEven(num1){
+    if (b % 2 == 0){
+        return ("Even")
+    } else {
+        return ("Odd")
+    }
+}
+
+var b = oddOrEven(457)
+console.log(b)
+
+function length(string){
+    if (string.length <= 20){
+        return (string + string)
+    } else {
+        return 
+    }
+}
+
+var c = length("trying")
+console.log(c)
